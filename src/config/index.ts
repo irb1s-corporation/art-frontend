242fd9185d92a3bc76c7b76800d5bd138cad507a
irb1s <kolyirb1s@gmail.com>
2021-11-23 10:53:01 +0500

    Commit

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,10 +4,10 @@ const rootUrl = () => {
     } else if (document.location.host === 'localhost:3000') {
         return 'http://localhost:5000/'
     } else if (document.location.host === 'irb1s.ru') {
-        return 'https://irb1s.ru:5000/';
+        return ' http://www.irb1s.ru:5000/';
     } else {
         console.log('Error, the address connection is not defined')
-        return 'https://www.irb1s.ru:5000/';
+        return 'http://www.irb1s.ru:5000/';
     }
 }
 
